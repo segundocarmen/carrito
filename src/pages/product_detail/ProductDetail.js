3a4a79fac24067a61a4a9c0e9cb6736da50f14a9
segundo carmen <segundocardom@gmail.com>
2018-10-20 23:27:46 -0500

    subiendo scaffold

---
+++ src/pages/product_detail/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class ProductDetail extends React.Component{
+	render(){
+		return(
+			<h1>Esta es la sección de detalle de productos.</h1>
+		)
+	}
+}
+
+
+export default ProductDetail
---
+++ src/pages/products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class Products extends React.Component{
+	render(){
+		return(
+			<h1>Esta es la sección de productos.</h1>
+		)
+	}
+}
+
+export default Products
